6f2b543ee0b62e7afd9a771147148f11d110a70a
mayo-camp <107622636+mayo-camp@users.noreply.github.com>
2025-09-09 07:54:11 +0900

    Update bw-codeblock.lua

--- bw-codeblock.lua
+++ bw-codeblock.lua
@@ -1,22 +1,9 @@
--- Lua フィルター: コードブロック＆インラインコードの高度対応
+-- GitHub風 コードブロック & インラインコード 用 Pandoc Luaフィルタ
 
-local function escape_latex(str)
-  str = str:gsub("\\", "\\textbackslash{}")
-  str = str:gsub("([#%%&$_{}])", "\\%1")
-  return str
-end
-
--- コードブロック
 function CodeBlock(block)
-  local content = escape_latex(block.text)
-  -- 改行長い行にも対応する tcolorbox
-  return pandoc.RawBlock(
-    'latex',
-    string.format("\\begin{codeblock}\n%s\n\\end{codeblock}", content)
-  )
+  return pandoc.RawBlock("latex", "\\begin{codeblock}\n" .. block.text .. "\n\\end{codeblock}")
 end
 
--- インラインコード
-function Code(el)
-  return pandoc.RawInline('latex', string.format("\\inlinecode{%s}", escape_latex(el.text)))
+function Code(block)
+  return pandoc.RawInline("latex", "\\icode{" .. block.text .. "}")
 end
